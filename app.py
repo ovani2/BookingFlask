f5fdfdaad3c1179ccb45eb0968b00dcac07a7b55
ovani2 <vladyslav271211@gmail.com>
2026-05-29 18:09:48 +0300

    поправив

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_login import login_required
 from werkzeug.security import generate_password_hash, check_password_hash
-from database import db, User
+from database import db, User  # Твоя окрема база даних
 
 app = Flask(__name__)
 
@@ -17,8 +16,7 @@ with app.app_context():
 
 @app.route('/')
 def index():
-    return render_template('/login.html')
-
+    return render_template('index.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -27,19 +25,22 @@ def register():
         username = request.form['username']
         password = request.form['password']
 
-        if User.query.filter_by(username=username).first():
+        stmt = db.select(User).filter_by(username=username)
+        user_exists = db.session.execute(stmt).scalar_one_or_none()
+
+        if user_exists:
             flash('Username already exists')
             return redirect(url_for('register'))
 
-        hashed_password = generate_password_hash(password)
+        hashed_password = generate_password_hash(password, method='scrypt')
         new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
 
         flash('Registration successful')
-        return redirect(url_for('index'))
+        return redirect(url_for('login'))
 
-    return render_template('/register.html')
+    return render_template('register.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -48,7 +49,9 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        user = User.query.filter_by(username=username).first()
+        stmt = db.select(User).filter_by(username=username)
+        user = db.session.execute(stmt).scalar_one_or_none()
+        
         if user and check_password_hash(user.password, password):
             flash('Login successful')
             return redirect(url_for('index'))
@@ -56,7 +59,7 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
 
-    return render_template('/login.html')
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
